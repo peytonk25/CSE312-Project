4f8d3c64f64f343f0e0e9cffe2b6b1470ae9bcfe
fcap25 <fcaputz@gmail.com>
2023-04-12 21:17:06 -0400

    Update server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@socketio.on('ping')
+def handle_ping():
+    emit('pong', {'data': 'Pong!'})
+
+if __name__ == '__main__':
+    socketio.run(app)

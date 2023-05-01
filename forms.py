535be087f7c8918a07435ad100175363820e8c05
fcap25 <fcaputz@gmail.com>
2023-05-01 17:09:37 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,3 +2,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     display_name = StringField('Display Name', validators=[DataRequired(), Length(min=2, max=20)])
     submit = SubmitField('Sign Up')
+
+ class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    submit = SubmitField('Log In')

72672b872b8d0e3bb17995fe9eaa0b2d2c467c57
fcap25 <fcaputz@gmail.com>
2023-05-01 17:08:43 -0400

    Create routes.py

---
+++ routes.py
@@ -0,0 +1,14 @@
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, display_name=form.display_name.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+ 

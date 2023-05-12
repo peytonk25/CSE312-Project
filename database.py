7810df32519105ccc6c06dd217c6503ea22a9ece
peytonk25 <peytonke@buffalo.edu>
2023-05-11 21:17:25 -0400

    Database change

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-mongo_client = MongoClient('mongo')
-db = mongo_client['cse_312']
+mongo_client = MongoClient("mongodb+srv://doapps-1ed21c95-ed6b-4356-ad32-6f090b16b2f4:2kh0K5B4SU3Qq761@db-mongodb-nyc1-48915-f5ba4355.mongo.ondigitalocean.com/app_database?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-48915")
+db = mongo_client['app_database']
 users_coll = db['users_coll']

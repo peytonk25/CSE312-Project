fed1d7da458246468699356cd994c5a3d8022839
peytonk25 <peytonke@buffalo.edu>
2023-05-11 22:32:17 -0400

    MongoClient

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-mongo_client = MongoClient("mongodb+srv://doapps-1ed21c95-ed6b-4356-ad32-6f090b16b2f4:2kh0K5B4SU3Qq761@db-mongodb-nyc1-48915-f5ba4355.mongo.ondigitalocean.com/app_database?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-48915")
+mongo_client = MongoClient("mongodb+srv://doapps-1ed21c95-ed6b-4356-ad32-6f090b16b2f4:39bz4C70DLw6e12g@db-mongodb-nyc1-48915-f5ba4355.mongo.ondigitalocean.com/admin?authSource=admin&tls=true")
 db = mongo_client['app_database']
 users_coll = db['users_coll']
